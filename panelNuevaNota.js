5e5bc3c961e3df070fc59ebdf3495b46764642b4
Marcocuma <46998207+Marcocuma@users.noreply.github.com>
2019-12-07 12:54:59 +0100

    Esquema

---
+++ panelNuevaNota.js
@@ -0,0 +1,7 @@
+class panelNota{
+    constructor(alto, ancho){
+        this.panel=document.createElement("div");
+        this.panel.style.width=ancho;
+        this.panel.style.height=alto;
+    }
+}
